bad21662e88bec034e22346a1dffcd0c7587b2a6
Vikas Sahu <111991055+vikas14022000@users.noreply.github.com>
2023-10-08 15:19:39 +0530

    Update settings1.py

--- a/a/settings.py
+++ a/a/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9!)*^&5c2@t!&&sdp9kv@vq08yj_iyf(-3cod#ujic$&$zb_^3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["51.20.9.240"]
 
 
 # Application definition
